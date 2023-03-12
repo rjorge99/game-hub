9c8085c05bb12223c6397173c88b5922fbcda2f4
Jorge Reyes <rjorge99@hotmail.com>
2023-03-12 10:55:02 -0600

    Highlight selectet genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,9 +4,10 @@ import getCroppedImageUrl from '../services/imageUrl';
 
 interface Props {
     onSelectGenre: (genre: Genre) => void;
+    selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
     const { data, isLoading, error } = useGenres();
 
     if (error) return null;
@@ -23,6 +24,9 @@ const GenreList = ({ onSelectGenre }: Props) => {
                             borderRadius={8}
                         />
                         <Button
+                            fontWeight={
+                                genre.id === selectedGenre?.id ? 'bold' : 'normal'
+                            }
                             fontSize='lg'
                             variant='link'
                             onClick={() => onSelectGenre(genre)}>
